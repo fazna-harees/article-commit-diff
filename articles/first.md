6572df126856fe6d05356663fa608358b647d063
fazna-harees <fasnahareesfasna@gmail.com>
2022-03-27 12:51:41 +0530

    added first

---
+++ articles/first.md
@@ -0,0 +1,14 @@
+---
+title: ERC-20 Tutorial
+author: Omer Furkan
+date: December 2022
+slug: erc20
+category: blockchain
+subcategory: tokens
+---
+
+## Blah
+
+### Hello world
+
+hfaifh faiwofhiawhfo heihe oerhoerheoirh or ejro
---
+++ convert-to-json.js
@@ -0,0 +1,88 @@
+/* eslint-disable consistent-return */
+const path = require("path");
+const fs = require("fs");
+const axios = require('axios');
+
+const dirPath = path.join(__dirname, "./articles");
+const postlist = [];
+
+const forEachPost = (file, i) => {
+    const obj = {};
+      let post;
+      // eslint-disable-next-line @typescript-eslint/no-shadow
+      fs.readFile(`${dirPath}/${file}`, "utf8", (err, contents) => {
+        const getMetadataIndices = (acc, elem, index) => {
+          if (/^---/.test(elem)) {
+            acc.push(index);
+          }
+          console.log(err)
+          return acc;
+        };
+        
+        const parseMetadata = ({ lines , metadataIndices }) => {
+          if (metadataIndices.length > 0) {
+            const metadata = lines.slice(metadataIndices[0] + 1, metadataIndices[1]);
+            metadata.forEach((line) => {
+              // eslint-disable-next-line prefer-destructuring
+              obj[line.split(": ")[0]] = line.split(": ")[1].slice(0,-1);
+            });
+            console.log(obj);
+            return obj;
+          }
+        };
+
+        const parseContent = ({ lines, metadataIndices }) => {
+          let newLines = lines;
+          if (metadataIndices.length > 0) {
+            newLines = lines.slice(metadataIndices[1] + 1, lines.length);
+          }
+          return newLines.join("\n");
+        };
+
+        const lines = contents.split("\n");
+        const metadataIndices = lines.reduce(getMetadataIndices, []);
+        const metadata = parseMetadata({ lines, metadataIndices });
+        const content = parseContent({ lines, metadataIndices });
+
+        post = {
+          title: metadata.title ? metadata.title : "No title given",
+          author: metadata.author ? metadata.author : "No author given",
+          description: metadata.description || "No date given",
+          category: metadata.category || "NULL",
+          slug: metadata.slug || "error",
+          subcategory: metadata.subcategory || "NULL",
+          content: content || "No content given",
+        };
+
+        postlist.push(post);
+        if (i === files.length - 1) {
+          const data = JSON.stringify(postlist);
+          fs.writeFileSync("src/posts.json", data);
+        }
+      });
+}
+const getPosts = () => {
+  fs.readdir(dirPath, (err, files) => {
+    if (err) {
+      return console.log(`failed${err}`);
+    }
+    files.forEach((file, i) => {
+    forEachPost(file,i)
+    });
+  });
+};
+
+// getPosts();
+
+const getCommits = async() => {
+    const commits = await axios.get('https://api.github.com/repos/fazna-harees/article-commit-diff/commits')
+    const last_commit = commits[commits.length -1 ]
+    console.log(last_commit)
+    const last_tree = await axios.get(last_commit.commit.tree.url)
+    console.log(last_tree.tree)
+    last_tree.tree.forEach(i => {
+        console.log(i.path)
+    })
+}
+
+getCommits()
